a0ed90d938289bf2a71ad348073fd289f5847319
Tommy <t.a.britnell@gmail.com>
2022-10-05 17:36:42 +0200

    empty example

--- index.js
+++ index.js
@@ -1,30 +1,14 @@
-// React is loaded and is available as React and ReactDOM
-// imports should NOT be used
-
-const Message = () => {
-  const [open, setOpen] = React.useState(false);
+const Test = () => {
+  const [state] = React.useState(0);
 
   return (
-    <React.Fragment>
-      <a
-        href="#"
-        onClick={(ev) => {
-          ev.preventDefault();
-          setOpen(!open);
-        }}
-      >
-        Want to buy a new car?
-      </a>
-      {open ? <p>Call +11 22 33 44 now!</p> : null}
-    </React.Fragment>
+    <div>
+      <h1>Demo - {state}</h1>
+    </div>
   );
 };
 
 document.body.innerHTML = "<div id='root'> </div>";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<Message />, rootElement);
-
-console.log("Before click: " + rootElement.innerHTML);
-document.querySelector("a").click();
-console.log("After click: " + rootElement.innerHTML);
+ReactDOM.render(<Test />, rootElement);
